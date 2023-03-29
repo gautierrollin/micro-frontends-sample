fb14307470da0b68dfb2f870c2b2a3716e85585c
Gautier Rollin <contact@gautierrollin.com>
2023-03-29 18:27:12 +0200

    feat: use `i18next` in `mfe-settings`

--- mfe-settings/src/Root.jsx
+++ mfe-settings/src/Root.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import i18next from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18next
+  .use(initReactI18next)
+  .use({
+    type : "backend",
+    read(language, namespace, callback) {
+      import(`./locales/${language}/${namespace}.json`)
+        .then((resources) => {
+          callback(null, resources);
+        })
+        .catch((error) => {
+          callback(error, null);
+        });
+    }
+  })
+  .init({
+    lng : "fr",
+    fallbackLng : "fr-FR",
+
+    react : {
+      useSuspense: false,
+    },
+
+    interpolation : {
+      escapeValue : false
+    }
+  });
 
 function Root() {
+  const { t } = useTranslation("root");
+
   return (
     <section>
-      <h1>User Settings</h1>
-      Name: {global.root.context.userName}
+      <h1>{t("userSettings")}</h1>
+      {t("name")}: {global.root.context.userName}
     </section>
   );
 }
